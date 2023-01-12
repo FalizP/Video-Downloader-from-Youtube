c94cd655249e8ca5011abe4a3829e7d17ce596ad
Luiz Palma <luizpalma16@gmail.com>
2023-01-12 00:09:01 -0300

    First Commit

---
+++ app.py
@@ -0,0 +1,65 @@
+import pytube as pt
+from time import time
+from colorama import Fore as fc
+from getDownloadPath import getDownloadPath
+
+#https://www.youtube.com/watch?v=HXYNW0ft5o4
+
+class videoDownloader():
+    _url = ''
+    _selectedVideo = ''
+    _title = ''
+    _videoObject = ''
+
+
+    def __init__(self, url):
+        while True:
+            try:
+                self._url = pt.YouTube(url)
+            except:
+                print(f'{fc.RED}Link Inválido...{fc.RESET}')
+                break
+
+            try:
+                self._selectedVideo = self._url.streams.filter(file_extension = 'mp4').get_highest_resolution()
+                self._title = self._selectedVideo.title
+                self._videoObject = {
+                    'Url' : self._url,
+                    'itag': self._selectedVideo.itag,
+                    'Title' : self._title
+                }
+            except:
+                print(fc.RED + 'Erro')
+            finally:
+                break
+
+
+    def __del__(self):
+        print(f'{fc.CYAN}Rotina de downloads finalizada...{fc.RESET}')
+        time.sleep(5)
+
+
+    def startDownload(self):
+        while True:
+            ans = input(f'{fc.MAGENTA}Confirmar dowload de: {fc.GREEN}{self._videoObject.get("Title")}{fc.MAGENTA} - [s] ou [n]:{fc.YELLOW} ')
+            ans = ans.lower().strip()
+            if ans == 's':
+                isConfirmed = True
+                break
+            elif ans == 'n':
+                isConfirmed = False
+                print(f'{fc.CYAN}Download cancelado...{fc.RESET}')
+                break
+
+        if isConfirmed:
+            try:
+                self._selectedVideo.download(getDownloadPath())
+                print(f'{fc.GREEN}Download Iniciado...')
+                print(f'Video..............: {self._videoObject.get("Title")}\n')
+            except:
+                print(f'{fc.RED}Download falhou...{fc.RESET}')
+
+
+url = input(f'{fc.MAGENTA}Insira o link do video:{fc.YELLOW} ')
+video = videoDownloader(url)
+video.startDownload()
---
+++ getDownloadPath.py
@@ -0,0 +1,14 @@
+import os, glob
+
+# Code done by someone in Stackoverflow
+# Thank you, mr. of Stackoverflow :)
+
+def getDownloadPath():
+    if os.name == 'nt':
+        import winreg
+        sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
+        downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
+            location = winreg.QueryValueEx(key, downloads_guid)[0]
+        return location
+    return os.path.join(os.path.expanduser('~'), 'downloads')
